898ad476b4a7aa6e73067b1c99524cf5e8a8f286
Сперанская Валерия <klena.huf@bk.ru>
2022-10-27 18:20:59 +0300

    Комментарий, описывающий изменения

---
+++ yatube/posts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'title' : title,
+        'posts' : posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'Лев Толстой - зеркало русской революции.'
+    text = 'Группа тайных поклонников графа.'
+    context = {
+        'text': text,
+        'title' : title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+ 
